862a9c24f61ba13329d519b1fa3c4f1deea60ac0
Zsofia Koma <au700510@uni.au.dk>
2021-09-10 11:46:16 +0200

    add shading of dtm

--- src/lidproc/qualcheck.R
+++ src/lidproc/qualcheck.R
@@ -4,7 +4,7 @@ library(mapview)
 library(future)
 
 # Set working directory
-workingdirectory="C:/_Koma/_PostDoc/Paper1/2_Dataprocess/qualityCheck/"
+workingdirectory="G:/My Drive/_Aarhus/Paper1/2_Dataprocessing/qualityCheck/"
 setwd(workingdirectory)
 
 #unzip(paste("PUNKTSKY_623_59_TIF_UTM32-ETRS89.zip",sep=""))
@@ -21,14 +21,29 @@ opt_filter(ctg) <- "-drop_class 7 18"
 opt_chunk_size(ctg) <- 0
 opt_chunk_buffer(ctg) <- 5
 
-opt_output_files(ctg) <-  paste("C:/_Koma/_PostDoc/Paper1/2_Dataprocess/qualityCheck/{*}_norm")
+opt_output_files(ctg) <-  paste("G:/My Drive/_Aarhus/Paper1/2_Dataprocessing/qualityCheck/{*}_norm")
 ctg_norm <- normalize_height(ctg, tin())
 
-opt_output_files(ctg) <-  paste("C:/_Koma/_PostDoc/Paper1/2_Dataprocess/qualityCheck/{*}_dtm")
+opt_output_files(ctg) <-  paste("G:/My Drive/_Aarhus/Paper1/2_Dataprocessing/qualityCheck/{*}_dtm")
 dtm <- grid_terrain(ctg, 10, tin())
+
+dtm_files <- dir(workingdirectory,pattern="*_dtm.tif")
+
+for (i in 1:length(dtm_files)) {
+  
+  print(dtm_files[i])
+  dtm=raster(dtm_files[i])
+  
+  slope <- terrain(dtm, opt='slope')
+  aspect <- terrain(dtm, opt='aspect')
+  
+  dtm_hillshade <- hillShade(slope,aspect,40, 270)
+  writeRaster(dtm_hillshade,paste(workingdirectory,substr(dtm_files[i],1,25),"_hsd.tif",sep=""),overwrite=TRUE)
+  
+}
 
 opt_filter(ctg_norm) <- "-keep_class 3 4 5"
 
-opt_output_files(ctg_norm) <-  paste("C:/_Koma/_PostDoc/Paper1/2_Dataprocess/qualityCheck/{*}_hmean")
+opt_output_files(ctg_norm) <-  paste("G:/My Drive/_Aarhus/Paper1/2_Dataprocessing/qualityCheck/{*}_hmean")
 hmean <- grid_metrics(ctg_norm, ~mean(Z), 10)
 
