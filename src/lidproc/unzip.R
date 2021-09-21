a71e514efdcab76b38094f166b4d3639e06666e0
Zsofia Koma <au700510@uni.au.dk>
2021-09-21 11:41:12 +0200

    make separate unzip file

---
+++ src/lidproc/unzip.R
@@ -0,0 +1,7 @@
+# Set working directory
+#inputdirectory="O:/Nat_Ecoinformatics/B_Read/Denmark/Elevation/LiDAR/2019/laz/ZIPdownload/"
+outputdirectory="O:/Nat_Ecoinformatics-tmp/au700510/unzipped_dir2019/"
+setwd(outputdirectory)
+
+ziplist=list.files(path=inputdirectory,pattern = "*.zip",full.names = TRUE)
+sapply(ziplist, unzip)
