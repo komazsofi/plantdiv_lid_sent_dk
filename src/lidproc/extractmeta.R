f53f30632fb5533fae86a563bac2a79755de2d5b
Zsofia Koma <au700510@uni.au.dk>
2021-09-30 17:01:25 +0200

    add pdens, crs finalize

--- src/lidproc/extractmeta.R
+++ src/lidproc/extractmeta.R
@@ -17,8 +17,9 @@ setwd("C:/_Koma/LAStools/LAStools/bin/")
 
 filelist=list.files(path=outputdirectory, pattern="\\.laz$", full.name=TRUE, include.dirs=TRUE, recursive=TRUE)
 
-lasinfo <- data.frame(matrix(ncol = 9, nrow = 0))
-x <- c("BlockID","FileName", "wkt_astext","NumPoints","MinGpstime", "MaxGpstime","Year","Month","Day")
+lasinfo <- data.frame(matrix(ncol = 23, nrow = 0))
+x <- c("BlockID","FileName", "wkt_astext","NumPoints","MinGpstime", "MaxGpstime","Year","Month","Day","zmin","zmax","maxRetNum","maxNumofRet","minClass","maxClass",
+       "minScanAngle","maxScanAngle","FirstRet","InterRet","LastRet","SingleRet","allPointDens","lastonlyPointDens")
 colnames(lasinfo) <- x
 
 ## set up parameters for the parallel process
@@ -30,11 +31,11 @@ registerDoParallel(cl)
 lasinfo <- foreach(i=1:length(filelist), .combine = rbind, .packages = c("sf")) %dopar% {
   print(i)
   
-  tmp <- system(paste("lasinfo.exe ",filelist[i]," -stdout",sep=""), intern=TRUE, wait=FALSE)
+  tmp <- system(paste("lasinfo.exe ",filelist[i]," -stdout -compute_density",sep=""), intern=TRUE, wait=FALSE)
   
   FileName <- paste(filelist[i])
   
-  BlockID <- substring(FileName,66,73)
+  BlockID <- substring(FileName,66,73) #needs to be adjust based on the used path!!!!
   
   NumPoints_str <- tmp[(grep(pattern = "  number of point records", tmp))]
   NumPoints<-as.numeric(unlist(strsplit(NumPoints_str, split=" "))[10])
@@ -55,11 +56,45 @@ lasinfo <- foreach(i=1:length(filelist), .combine = rbind, .packages = c("sf")) 
   MinGpstime=as.character(as.POSIXct(as.numeric(gpsrange2[4])+1000000000,origin="1980-01-06"))
   MaxGpstime=as.character(as.POSIXct(as.numeric(gpsrange2[5])+1000000000,origin="1980-01-06"))
   
-  Year=substring(MinGpstime,1,4)
-  Month=substring(MinGpstime,6,7)
-  Day=substring(MinGpstime,9,10)
+  Year=substring(MaxGpstime,1,4)
+  Month=substring(MaxGpstime,6,7)
+  Day=substring(MaxGpstime,9,10)
   
-  newline <- cbind(BlockID,FileName, wkt_astext,NumPoints, MinGpstime, MaxGpstime,Year,Month,Day)
+  zmin=as.numeric(unlist(strsplit(mins, " * "))[8])
+  zmax=as.numeric(unlist(strsplit(maxs, " * "))[8])
+  
+  RetNum_str <- tmp[(grep(pattern = "  return_number", tmp))]
+  maxRetNum<-as.numeric(unlist(strsplit(RetNum_str, split=" "))[20])
+  
+  NumofRet_str <- tmp[(grep(pattern = "  number_of_returns", tmp))]
+  maxNumofRet<-as.numeric(unlist(strsplit(NumofRet_str, split=" "))[16])
+  
+  Class_str <- tmp[(grep(pattern = "  classification", tmp))]
+  minClass <-as.numeric(unlist(strsplit(Class_str, split=" "))[9])
+  maxClass <-as.numeric(unlist(strsplit(Class_str, split=" "))[18])
+  
+  ScanAngle_str <- tmp[(grep(pattern = "  scan_angle_rank", tmp))]
+  minScanAngle <-as.numeric(unlist(strsplit(ScanAngle_str, split=" "))[6])
+  maxScanAngle <-as.numeric(unlist(strsplit(ScanAngle_str, split=" "))[15])
+  
+  FirstRet_str <- tmp[(grep(pattern = "number of first returns:", tmp))]
+  FirstRet <-as.numeric(unlist(strsplit(FirstRet_str, split=" "))[12])
+  
+  InterRet_str <- tmp[(grep(pattern = "number of intermediate returns:", tmp))]
+  InterRet <-as.numeric(unlist(strsplit(InterRet_str, split=" "))[5])
+  
+  LastRet_str <- tmp[(grep(pattern = "number of last returns:", tmp))]
+  LastRet <-as.numeric(unlist(strsplit(LastRet_str, split=" "))[13])
+  
+  SingleRet_str <- tmp[(grep(pattern = "number of single returns:", tmp))]
+  SingleRet <-as.numeric(unlist(strsplit(SingleRet_str, split=" "))[11])
+  
+  PointDens_str <- tmp[(grep(pattern = "point density:", tmp))]
+  allPointDens <-as.numeric(unlist(strsplit(PointDens_str, split=" "))[5])
+  lastonlyPointDens <-as.numeric(unlist(strsplit(PointDens_str, split=" "))[8])
+  
+  newline <- cbind(BlockID,FileName,wkt_astext,NumPoints,MinGpstime,MaxGpstime,Year,Month,Day,zmin,zmax,maxRetNum,maxNumofRet,minClass,maxClass,
+                   minScanAngle,maxScanAngle,FirstRet,InterRet,LastRet,SingleRet,allPointDens,lastonlyPointDens)
   
   newline
 }
@@ -70,6 +105,7 @@ stopCluster(cl)
 
 lasinfo_df=as.data.frame(lasinfo)
 df = st_as_sf(lasinfo_df, wkt = "wkt_astext")
+st_crs(df) <- 25832 
 st_write(df, paste(outputdirectory,"lasinfo.shp",sep=""))
 
 
