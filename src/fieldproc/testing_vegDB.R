bd85bc11835af05647887ffe0ff718d90b6a51d5
Zsofia Koma <komazsofi@gmail.com>
2021-10-05 16:12:32 +0200

    add mapo voisualization and diff filters

--- src/fieldproc/testing_vegDB.R
+++ src/fieldproc/testing_vegDB.R
@@ -6,6 +6,8 @@ abiotiske=read.csv("G:/My Drive/_Aarhus/Paper1/1_Datasets/Fielddata/Novana/alled
 cover=read.csv("G:/My Drive/_Aarhus/Paper1/1_Datasets/Fielddata/Novana/alledata-cover.csv",sep=",")
 frekvens=read.csv("G:/My Drive/_Aarhus/Paper1/1_Datasets/Fielddata/Novana/alledata-frekvens.csv",sep=",")
 
+Denmark <- readRDS("G:/My Drive/_Aarhus/Paper1/1_Datasets/DK_Shape.rds")
+
 # How many plots were measured across years
 
 abiotiske_perplots <- abiotiske %>% group_by(plot) %>% summarise(n = n())
@@ -19,14 +21,29 @@ veg_db<- abiotiske[c("site", "plot", "year","UTMx","UTMy")]
 #veg_db_plantdiv$measurefreq <- merge(veg_db_plantdiv, abiotiske_perplots, by="plot")
 veg_db_plantdiv<- left_join(veg_db, abiotiske_perplots_df, by="plot")
 
-# calculate species richness
+# calculate plant diversity indices
 
 veg_db_plantdiv$richness <- rowSums(frekvens[,4:2180])
-ggplot(data=veg_db_plantdiv[(veg_db_plantdiv$site==1),],aes(x=year,y=richness,group=plot,color=as.factor(plot)))+geom_line()+geom_point()
 
-# export to QGIS
+# interesting data
 
 veg_db_plantdiv_comp=veg_db_plantdiv[!(is.na(veg_db_plantdiv$UTMx) | veg_db_plantdiv$UTMx=="" | veg_db_plantdiv$UTMx=="mv"), ]
 
-veg_db_plantdiv_sf <- st_as_sf(veg_db_plantdiv_comp, coords = c("UTMx", "UTMy"), crs = 25832)
+veg_db_plantdiv_mintimes=veg_db_plantdiv_comp[(veg_db_plantdiv_comp$n>4),]
+#print(unique(veg_db_plantdiv_mintimes$site))
+
+# visualize
+
+ggplot(data=veg_db_plantdiv_mintimes[(veg_db_plantdiv_mintimes$site==45),],aes(x=year,y=richness,group=plot,color=as.factor(plot)))+geom_line()+geom_point()
+
+# Map visualizations
+
+veg_db_plantdiv_mintimes_perplots <- veg_db_plantdiv_mintimes %>% group_by(plot) %>% summarise(UTMx=max(UTMx),UTMy=max(UTMy),site=max(site),n=max(n),meanrich_overtime=mean(richness))
+veg_db_plantdiv_mintimes_perplots_sf <- st_as_sf(veg_db_plantdiv_mintimes_perplots, coords = c("UTMx", "UTMy"), crs = 25832)
+
+ggplot()+geom_sf(data = Denmark)+geom_sf(data = veg_db_plantdiv_mintimes_perplots_sf, aes(color = n))
+
+# export to QGIS
+
+veg_db_plantdiv_sf <- st_as_sf(veg_db_plantdiv_mintimes, coords = c("UTMx", "UTMy"), crs = 25832)
 st_write(veg_db_plantdiv_sf, "G:/My Drive/_Aarhus/Paper1/1_Datasets/Fielddata/Novana/veg_db_plantdiv_comp.shp")
