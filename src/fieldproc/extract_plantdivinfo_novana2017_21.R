acf5c688ddd28f2b8c7aebe4d730765059d0b7b1
Zsofia Koma <komazsofi@gmail.com>
2022-01-11 09:51:59 +0100

    add maps

--- src/fieldproc/extract_plantdivinfo_novana2017_21.R
+++ src/fieldproc/extract_plantdivinfo_novana2017_21.R
@@ -1,16 +1,39 @@
-library(ggplot2)
+library(sf)
+library(tidyverse)
 
+Denmark <- readRDS("O:/Nat_Ecoinformatics-tmp/au700510_2022_1/fielddata/DK_Shape.rds")
 novana_17_21=readRDS("O:/Nat_Ecoinformatics-tmp/au700510_2022_1/fielddata/Novana2/Novana_test_2017-2021.rds")
+
+# Convert attributes
 novana_17_21$YEAR <- format(novana_17_21$STARTDATO, format="%Y")
 
 print(unique(novana_17_21$YEAR))
 print(unique(novana_17_21$NATURTYPE))
 
-## histograms 
+novana17=novana_17_21[novana_17_21$YEAR==2017,]
 
-# habitat types per year
+## Histograms 
+
+# Habitat types per year
 
 ggplot(novana_17_21, aes(x = YEAR, fill = NATURTYPE)) +
-  geom_histogram(stat="count",position = "dodge")
+  geom_histogram(stat="count",position = "dodge")+theme_bw()
 
-## maps (where these points located - density within grid?)
+## Maps (where these points located - density within grid?)
+
+ggplot()+geom_sf(data = Denmark)+
+  geom_sf(data = novana_17_21[novana_17_21$YEAR==2017,], aes(color = NATURTYPE),size=2)+theme_bw()
+
+ggplot()+geom_sf(data = Denmark)+
+  geom_sf(data = novana_17_21[novana_17_21$YEAR==2018,], aes(color = NATURTYPE),size=2)+theme_bw()
+
+ggplot()+geom_sf(data = Denmark)+
+  geom_sf(data = novana_17_21[novana_17_21$YEAR==2019,], aes(color = NATURTYPE),size=2)+theme_bw()
+
+ggplot()+geom_sf(data = Denmark)+
+  geom_sf(data = novana_17_21[novana_17_21$YEAR==2020,], aes(color = NATURTYPE),size=2)+theme_bw()
+
+ggplot()+geom_sf(data = Denmark)+
+  geom_sf(data = novana_17_21[novana_17_21$YEAR==2021,], aes(color = NATURTYPE),size=2)+theme_bw()
+
+
