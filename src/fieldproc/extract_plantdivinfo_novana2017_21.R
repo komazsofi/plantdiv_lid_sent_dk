2ab721113cda12f1446990c25d6bffb20a652bf5
Zsofia Koma <komazsofi@gmail.com>
2022-01-13 11:33:03 +0100

    add expl. new extract

--- src/fieldproc/extract_plantdivinfo_novana2017_21.R
+++ src/fieldproc/extract_plantdivinfo_novana2017_21.R
@@ -2,6 +2,8 @@ library(sf)
 library(tidyverse)
 
 Denmark <- readRDS("O:/Nat_Ecoinformatics-tmp/au700510_2022_1/fielddata/DK_Shape.rds")
+
+## NOVANA extraction 1 (novana_2017_2021)
 novana_17_21=readRDS("O:/Nat_Ecoinformatics-tmp/au700510_2022_1/fielddata/Novana2/Novana_test_2017-2021.rds")
 
 # Convert attributes
@@ -9,8 +11,6 @@ novana_17_21$YEAR <- format(novana_17_21$STARTDATO, format="%Y")
 
 print(unique(novana_17_21$YEAR))
 print(unique(novana_17_21$NATURTYPE))
-
-novana17=novana_17_21[novana_17_21$YEAR==2017,]
 
 ## Histograms 
 
@@ -36,4 +36,18 @@ ggplot()+geom_sf(data = Denmark)+
 ggplot()+geom_sf(data = Denmark)+
   geom_sf(data = novana_17_21[novana_17_21$YEAR==2021,], aes(color = NATURTYPE),size=2)+theme_bw()
 
+## NOVANA extraction 2 (Dataset2005_2021)
 
+nov_extr2=read_sf("O:/Nat_Ecoinformatics-tmp/au700510_2022_1/fielddata/Novana2/Dataset2005_2021.shp")
+
+nov_extr2$Year2 <- format(nov_extr2$STARTDA, format="%Y")
+
+# Visualization
+
+ggplot(nov_extr2, aes(x = Year2, fill = PROGRAM)) +
+  geom_histogram(stat="count",position = "dodge")+theme_bw()
+
+ggplot(data = nov_extr2)+
+  geom_sf()+
+  facet_wrap(facets = vars(Year2))+
+  theme_bw()
