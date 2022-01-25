3c2a646b9618b252fa5dd7c63e76a8cc0a4faed3
Zsofia Koma <komazsofi@gmail.com>
2022-01-25 15:41:49 +0100

    get date of flight from max length polyline

--- src/lidproc2/extract_time_fromtrajectory.R
+++ src/lidproc2/extract_time_fromtrajectory.R
@@ -1,19 +1,57 @@
 library(sf)
+library(plyr)
+library(dplyr)
 
 # Set global variables
-wd="O:/Nat_Ecoinformatics-tmp/au700510_2022_1/lidarprocess/extract_info_from_flightlines/"
+wd="O:/Nat_Ecoinformatics-tmp/au700510_2022_1/lidarprocess/extract_info_from_flightlines/unziped_KMS2007_traj/"
 setwd(wd)
+
+filelist=list.files(pattern = "*.shp")
 
 # Import 
 
-DHM2017 = read_sf("DHM2007_20211102_2342.shp")
-scanline = read_sf("O:/Nat_Ecoinformatics-tmp/au700510_2022_1/lidarprocess/extract_info_from_flightlines/unziped_KMS2007_traj/scan_604_68.shp")
+DHM2017 = read_sf("O:/Nat_Ecoinformatics-tmp/au700510_2022_1/lidarprocess/extract_info_from_flightlines/DHM2007_20211102_2342.shp")
 
-st_crs(scanline) = st_crs(DHM2017)
+# Intersect the scan lines with the polygons
 
-# intersection
-int = st_intersection(DHM2017, scanline)
-plot(int)
+for (i in filelist) {
+  print(i)
+  
+  scanline = read_sf(i)
+  st_crs(scanline) = st_crs(DHM2017)
+  
+  scanline$length = st_length(scanline$geometry)
+  linepol = st_intersection(scanline, DHM2017)
+  
+  linepol$lpercent = 100 * (st_length(linepol)/linepol$length)
+  
+  # intersection
+  int = st_intersection(DHM2017, linepol)
+  int_sel=int[,c(1,66,30)]
+  int_sel_df <- int_sel %>% st_set_geometry(NULL)
+  
+  write.csv(int_sel_df,paste("O:/Nat_Ecoinformatics-tmp/au700510_2022_1/lidarprocess/extract_info_from_flightlines/",gsub('.{4}$', '', i),".csv"))
+}
 
-int = st_intersection(scanline, DHM2017)
-plot(int)
+# Import back csv files into one
+
+filenames <- list.files(path = "O:/Nat_Ecoinformatics-tmp/au700510_2022_1/lidarprocess/extract_info_from_flightlines/", pattern = "*.csv", full.names=TRUE)
+csvs <- ldply(filenames, read.csv)
+
+# select the ones which are the longest line within the intersected polygon
+
+csvs_grouped=csvs %>% group_by(BlockID) %>% top_n(1, lpercent)
+
+# format date into year, month, day
+
+csvs_grouped <- transform(csvs_grouped, date = as.Date(as.character(SCANDATE), "%Y%m%d"))
+csvs_grouped$year <- format(csvs_grouped$date, format="%Y")
+csvs_grouped$month <- format(csvs_grouped$date, format="%m")
+csvs_grouped$day <- format(csvs_grouped$date, format="%d")
+
+# add this info to polygon
+
+joined_DHM2017 = DHM2017 %>% left_join(csvs_grouped, by = "BlockID")
+st_write(joined_DHM2017, "O:/Nat_Ecoinformatics-tmp/au700510_2022_1/lidarprocess/extract_info_from_flightlines/DHM2017_joined_maxlengthtrajdate.shp")
+
+
